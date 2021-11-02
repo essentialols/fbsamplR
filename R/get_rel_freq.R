0ae0fa4455ae54559e6c64af46601207a330308f
essentialols <ingmarsturm@gmail.com>
2021-11-02 13:50:32 -0700

    Updated documentation.

--- R/get_rel_freq.R
+++ R/get_rel_freq.R
@@ -1,3 +1,18 @@
+#' Get relative frequencies
+#'
+#' This function takes a quota tibble or dataframe as its input and returns a tibble as its output.
+#' The function assumes that one column, the "location" column, includes the location of your quota, e.g. a country, county, or city. This can also just be a single value for all rows.
+#' The freq_raw argument encodes the raw number of people.
+#'
+#' @param quota dataframe or tibble
+#' @param location factor or character
+#' @param freq_raw numeric
+#'
+#' @return
+#' @export
+#'
+#' @examples
+
 get_rel_freq <- function(quota, location = name, freq_raw = value){
   location <- rlang::enquo(location)
   freq_raw <- rlang::enquo(freq_raw)
